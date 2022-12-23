4a382a918bbd68813b19374219db358afa365e62
CamilaAchuri <camilaachury.4@gmail.com>
2022-12-23 00:29:10 -0300

    add click function

---
+++ R/04_write_html.R
@@ -0,0 +1,44 @@
+#' @export
+write_html <- function(data, dic, click, class_title, class_body, id = NULL, ...) {
+  if (is.null(data)) return()
+  if (is.null(dic)) return()
+  if (is.null(click)) return()
+  data_click <- data_filter(data = data,
+                            dic = dic,
+                            var_inputs = click,
+                            .id = id)
+  data_click <- data_click |> dplyr::select(...)
+  info_click <- names(data_click)
+
+  htmltools::HTML(
+    paste0(
+      purrr::map(unique(data_click[[info_click[1]]]), function(x) {
+        df <- data_click |> dplyr::filter(!!dplyr::sym(info_click[1]) %in% x)
+        htmltools::HTML(
+          paste0(
+
+            htmltools::div(class = class_title,
+                htmltools::HTML(
+                  paste0(
+                    "<q>", x, "</q>")
+                )),
+            htmltools::div(class = class_body,
+                htmltools::HTML(
+                  paste0(
+                    purrr::map(names(df)[-1], function(v) {
+
+                      tx <- paste0("<div class = 'click-p'><div class = 'click-tl'>", v, ":</div> <div class = 'click-subtitle'>",
+                                   df[[v]], "</div></div>", collapse = "")
+                      if (v == "url") {
+                        tx <- paste0("<div style='display:flex;justify-content: space-between;'><div>",
+                                     "<a href=", df[[v]]," target='_blank'>Link &#8734;</a>")
+                      }
+                      tx
+                    }), collapse = "</br>" )
+                )
+            )
+          )
+        )
+      }), collapse = "</br>" )
+  )
+}
---
+++ tests/testthat/test-write_html.R
@@ -0,0 +1,17 @@
+test_that("Summary click in html", {
+  data <- iris
+  dic <- homodatum::create_dic(data)
+  names(data) <- dic$id
+  html_result <- write_html(data = data,
+                            dic = dic,
+                            click = list("species" = "setosa"),
+                            class_title = "click-title",
+                            class_body = "click-text",
+                            id = NULL,
+                            "species")
+  html_expect <- htmltools::HTML(
+    '<div class="click-title"><q>setosa</q></div><div class="click-text"></div>')
+
+  expect_equal(html_result, html_expect)
+
+})

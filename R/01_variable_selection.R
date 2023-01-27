a212bbb529f5811cd05ba23d14a58af19a21291f
Pablo Tiscornia <pablotisco@gmail.com>
2023-01-26 21:20:44 -0600

    add `data_selection()` documentation

--- R/01_variable_selection.R
+++ R/01_variable_selection.R
@@ -1,3 +1,23 @@
+#' @title Subset columns by their names
+#'
+#' @description The `data_select()` function is used to subset an specific columns from a data frame by their names or by defining them in a YAML configuration file. This function helps reducing code writing when building a shiny app.
+#'
+#' @param data A data frame
+#' @param path path to the YAML to be called if necessary. Default value is NULL.
+#' @param viz Specific type of viz when selecting by a YAML configuration file. Default value is NULL.
+#' @param ... One or more quoted expressions (variable names) separated by commas.
+#'
+#' @examples
+#'
+#' data <- iris
+#'
+#' data_result <- variable_selection(data = iris,
+#'                                   viz = NULL,
+#'                                   path = NULL,
+#'                                   "Sepal.Length", "Petal.Length")
+#'
+#'  names(data_result)
+#'
 #' @export
 variable_selection <- function(data, path = NULL, viz = NULL, ...) {
   if (is.null(data)) return()
